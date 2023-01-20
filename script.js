b2a0b2dfa42cf053f9b26249fdc9ae42439c76aa
Meraj78692 <106391224+Meraj78692@users.noreply.github.com>
2023-01-20 17:29:34 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+document.getElementById("intro").style.color = "violet";
+document.getElementById("intro").style.fontSize = "50px";
+document.getElementById("intro").style.margin = "100px 0 25px 500px";
+document.getElementById("p1").innerHTML = "Hello JavaScript";
+document.getElementById("p1").style.color = "blue";
+document.getElementById("p1").style.fontSize = "35px";
+document.body.style.backgroundColor = "lightyellow";
+document.getElementById("p1").style.margin = "20px 0 25px 650px";
